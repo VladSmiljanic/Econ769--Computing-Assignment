8a1060e0ccee5ebbe26b8fbcb05a8d149d49f008
VladSmiljanic <vladsmiljanic@gmail.com>
2017-05-09 14:13:16 -0400

    Updates
    You get an update, and you get an update!

--- Estimation.R
+++ Estimation.R
@@ -1,24 +1,89 @@
 library(haven)
-source <- read_dta("~/Desktop/Vlad's Stuff/Vlad's School/Econ769- Computing Assignment/017769exercise.dta")
+#source <- read_dta("~/Desktop/Vlad's Stuff/Vlad's School/Econ769- Computing Assignment/017769exercise.dta")
+source <- read_dta("C:/Users/vlads_000/Documents/GitHub/Econ769--Computing-Assignment/017769exercise.dta")
 require(plm)
 require(boot)
+tab<-data.frame(Model=c("Pooled","Between","Within","First Difference","Random Effect"))
+resamp<-function(data,replace,size){ #Resampling function
+  unique_ind<-unique(source$id) #Identifies the datas unique individuals
+  samp_ind<-sample(unique_ind,size=size,replace=replace) #Randomly samples over those individuals
+  do.call(rbind,lapply(samp_ind,function(x) source[source$id==x,])) #Returns data frame with those individuals observations over time.
+}
 
-pooledOLS <- plm(lnhr ~ lnwg, data = source, model = "pooling",index=c("id","year"))
-summary(pooledOLS)
-between <- plm(lnhr ~ lnwg, data = source, model = "between",index=c("id"))
+mydata<-resamp(source,replace=FALSE,size=266)
+alpha<-numeric()
+beta<-numeric()
+se<-numeric()
+se.b<-numeric()
 
-within <- plm(lnhr ~ lnwg, data = source, model = "within",effect="twoways")
-mean(fixef(within)) #The average of all the fixed effects in the model. 
+pooled<- plm(lnhr ~ lnwg, data = mydata, model = "pooling",index=c("id"))
+between <- plm(lnhr ~ lnwg, data = mydata, model = "between",index=c("id"))
+within <- plm(lnhr ~ lnwg, data = mydata, model = "within",index=c("id"),effect="individual")
+a.within<-mean(fixef(within)) 
+fd<-plm(lnhr ~ lnwg, data = mydata, model = "fd",index=c("id"))
+random<-plm(lnhr ~ lnwg, data = mydata, model = "random",index=c("id"),effect="individual")
 
-first_diff<-plm(lnhr ~ lnwg, data = source, model = "fd",index=c("id"))
+alpha[1]<-coef(pooled)[1]
+alpha[2]<-coef(between)[1]
+alpha[3]<-a.within
+alpha[4]<-coef(fd)[1]
+alpha[5]<-coef(random)[1]
+tab$alpha<-alpha
 
-rand_eff<-plm(lnhr ~ lnwg, data = source, model = "random",index=c("id","year"),effect="twoways")
-summary(rand_eff)
+beta[1]<-coef(pooled)[2]
+beta[2]<-coef(between)[2]
+beta[3]<-coef(within)[1]
+beta[4]<-coef(fd)[2]
+beta[5]<-coef(random)[2]
+tab$beta<-beta
 
-boot.rx <- function(data,i) {coef(lm(lnhr~lnwg,subset=i,data=data))}
-output.rx <- boot(source, boot.rx, 999)
 
-x <- rchisq(100,df=5)
-boot.fun <- function(mydat,i) mean(mydat[i])
-boot.out <- boot(x,boot.fun,1000)
-boot.out
+se[1]<-sqrt(vcov(pooled)[2][2])
+se[2]<-coef(between)[2]
+se[3]<-coef(within)[1]
+se[4]<-coef(fd)[2]
+se[5]<-coef(random)[2]
+tab$se<-se
+
+
+
+
+boot.pool<-function(mydat) coef(plm(lnhr ~ lnwg, data = mydat, model = "pooling",index=c("id")))[2]
+
+boot.between<-function(mydat) coef(plm(lnhr ~ lnwg, data = mydat, model = "between",index=c("id")))[2]
+
+boot.within<-function(mydat) coef(plm(lnhr ~ lnwg, data = mydat, model = "within",index=c("id"),effect="individual"))[1]
+
+boot.fd<-function(mydat) coef(plm(lnhr ~ lnwg, data = mydat, model = "fd",index=c("id")))[2]
+
+boot.re<-function(mydat) coef(plm(lnhr ~ lnwg, data = mydat, model = "random",index=c("id")))[2]
+
+boot.my<-function(myfun, R){
+  vec.coef<-numeric()
+  if (myfun=="pooling"){
+    for (i in 1:R){
+      vec.coef[i]<-boot.pool(resamp(source,replace=TRUE,size=532))
+    }  
+  } else if (myfun=="within"){
+    for (i in 1:R){
+      vec.coef[i]<-boot.within(resamp(source,replace=TRUE,size=532))
+    }
+  } else if (myfun=="between"){
+    for (i in 1:R){
+      vec.coef[i]<-boot.between(resamp(source,replace=TRUE,size=532))
+    }
+  } else if (myfun=="random"){
+    for (i in 1:R){
+      vec.coef[i]<-boot.re(resamp(source,replace=TRUE,size=532))
+    }
+  } else if (myfun=="fd"){
+    for (i in 1:R){
+      vec.coef[i]<-boot.fd(resamp(source,replace=TRUE,size=532))
+    }
+  }
+  print(paste("Bootstrapped Standard Error: ",round(sd(vec.coef),digits=3)))
+}
+
+boot.my("pooling",200)
+
+
